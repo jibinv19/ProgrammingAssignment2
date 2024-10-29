c7e927b46a9a97b8653a930624c7e128f9b0178a
Jibin Varghese <116850480+jibinv19@users.noreply.github.com>
2024-10-29 18:14:03 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+         # check if x is a matrix
+    if(is.matrix(x)) {
+        print("It is a matix, continue...")
+        m <- NULL
+        #print(paste0(cat("m1: "), m))
+        set <- function(y) {
+            x <<- y
+            m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+    } else {
+        print("x is not a matrix... input a matrix to use the function...")
+        return(message("The matrix is'n invertible."))
+    }
 
 }
 
@@ -12,4 +31,32 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+          m <- x$getinverse()
+    print(paste0(cat("cacheSolve m1: "), m))
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    print(paste0(cat("cacheSolve data: "), data))
+    m <- solve(data, ...)
+    print(paste0(cat("cacheSolve m1: "), m))
+    x$setinverse(m)
+    print(paste0(cat("cacheSolve x$setinverse(m): "), x$setinverse(m)))
+    m
+    print(paste0(cat("cacheSolve m: "), m))
 }
+
+
+# make matrix
+# Create a special matrix with initial values
+m <- makeCacheMatrix(matrix(c(1,2,3,4),ncol=2,nrow=2))
+
+# Calculate and cache the inverse
+inv1 <- cacheSolve(m)
+print(inv1)  # This will compute the inverse and output it
+
+# Get the cached inverse
+inv2 <- cacheSolve(m)
+print(inv2)  # This will fetch the cached inverse and output it
+
